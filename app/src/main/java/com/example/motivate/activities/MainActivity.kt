a97ea7638d6714cebca7a790c8a71fc51b8b51da
alkanoidev <alkanoidev@gmail.com>
2021-04-28 11:15:28 +0200

    implemented coroutines

--- app/src/main/java/com/example/motivate/activities/MainActivity.kt
+++ app/src/main/java/com/example/motivate/activities/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.example.motivate.activities
 
 import android.annotation.SuppressLint
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.motivate.models.ImagesModel
-import com.example.motivate.models.QuotesModel
 import com.example.motivate.R
 import com.example.motivate.activities.SplashScreen.Companion.context
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
+import com.example.motivate.models.ImagesModel
+import com.example.motivate.models.QuotesModel
+import com.example.motivate.util.Utils
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.Main
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageList:MutableList<ImagesModel>
     private lateinit var instanceQuotes: QuotesModel
     private lateinit var instanceImages: ImagesModel
+    private var tools: Utils = Utils()
 
     private val TAG: String = MainActivity::class.java.simpleName
 
@@ -49,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         authorView = findViewById(R.id.author)
         imageView = findViewById(R.id.imageView)
 
-        thread = Thread(Runnable {
+        /*thread = Thread(Runnable {
             val url:URL? = try {
                 URL(quotesApiURL)
             }catch (e: MalformedURLException){
@@ -57,7 +53,7 @@ class MainActivity : AppCompatActivity() {
                 null
             }
             url?.getString()?.apply {
-                quoteList = parseJsonData(this@apply)
+                quoteList = tools.parseJsonData(this@apply)
             }
             val url1: URL? = try {
                 URL(imagesURL)
@@ -66,51 +62,37 @@ class MainActivity : AppCompatActivity() {
                 null
             }
             url1?.getString()?.apply {
-                imageList = parseJsonImage(this@apply)
+                imageList = tools.parseJsonImage(this@apply)
             }
 
             runOnUiThread {
                 instanceImages = imageList[Random.nextInt(0, imageList.size - 1)]
-                instanceImages.download_url = "https://picsum.photos/id/${instanceImages.id}/${getScreenWidth()}/${getScreenHeight()}/?blur=5"
+                instanceImages.download_url = "https://picsum.photos/id/${instanceImages.id}/${tools.getScreenWidth()}/${tools.getScreenHeight()}/?blur=5"
                 Glide.with(context).load(instanceImages.download_url).into(imageView)
 
             }
         })
-        thread.start()
+        thread.start()*/
 
-    }
-
-    private fun URL.getString(): String {
-        val stream = openStream()
-        return try {
-            val r = BufferedReader(InputStreamReader(stream))
-            val result = StringBuilder()
-            var line: String?
-            while (r.readLine().also { line = it } != null) {
-                result.append(line).appendln()
-            }
-            result.toString()
-        }catch (e: IOException){
-            e.toString()
+        GlobalScope.launch(Dispatchers.IO) {
+            process()
+            setImage()
         }
+
+
+    }
+    private suspend fun process() = coroutineScope {
+        quoteList = async { tools.getQuotes() }.await()
+        imageList = async{ tools.getImages() }.await()
     }
 
-    private fun parseJsonData(data:String): MutableList<QuotesModel> {
-        val list:MutableList<QuotesModel> = Gson().fromJson<MutableList<QuotesModel>>(data)
-        list.forEach{
-            if(it.author==null){
-                it.author="Unknown"
-            }
-        }
-        return list
+    private suspend fun setImage() = withContext(Main){
+        instanceImages = imageList[Random.nextInt(0, imageList.size - 1)] //not initialized
+        instanceImages.download_url = "https://picsum.photos/id/" +
+                                      "${instanceImages.id}/${tools.getScreenWidth()}/" +
+                                      "${tools.getScreenHeight()}/?blur=5"
+        Glide.with(context).load(instanceImages.download_url).into(imageView)
     }
-
-    private fun parseJsonImage(data:String): MutableList<ImagesModel> {
-        val list:MutableList<ImagesModel> = Gson().fromJson<MutableList<ImagesModel>>(data)
-        return list
-    }
-
-    private inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object : TypeToken<T>() {}.type)
 
     fun update(view: View) {
         quoteView=findViewById(R.id.quote)
@@ -122,11 +104,4 @@ class MainActivity : AppCompatActivity() {
         authorView.text = instanceQuotes.author
     }
 
-    private fun getScreenWidth(): String {
-        return Resources.getSystem().displayMetrics.widthPixels.toString()
-    }
-
-    private fun getScreenHeight(): String {
-        return Resources.getSystem().displayMetrics.heightPixels.toString()
-    }
 }
---
+++ app/src/main/java/com/example/motivate/util/Utils.kt
@@ -0,0 +1,85 @@
+package com.example.motivate.util
+
+import android.content.res.Resources
+import com.example.motivate.models.ImagesModel
+import com.example.motivate.models.QuotesModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+
+class Utils {
+
+    private lateinit var quoteList: MutableList<QuotesModel>
+    private lateinit var imageList: MutableList<ImagesModel>
+
+    private val quotesApiURL: String = "https://type.fit/api/quotes"
+    private var imagesURL: String = "https://picsum.photos/v2/list"
+
+    suspend fun getQuotes(): MutableList<QuotesModel> = coroutineScope {
+        val url1: URL = async(Dispatchers.IO) {
+            URL(quotesApiURL)
+        }.await()
+
+        url1.getString().apply {
+            quoteList = async(Dispatchers.Default) { parseJsonData(this@apply) }.await()
+        }
+        return@coroutineScope quoteList
+
+    }
+
+    suspend fun getImages(): MutableList<ImagesModel> = coroutineScope {
+        val url: URL = async(Dispatchers.IO) {
+            URL(imagesURL)
+        }.await()
+
+        url.getString().apply {
+            imageList = async(Dispatchers.Default) { parseJsonImage(this@apply) }.await()
+        }
+        return@coroutineScope imageList
+
+    }
+
+    private fun URL.getString(): String {
+        val stream = openStream()
+        return try {
+            val r = BufferedReader(InputStreamReader(stream))
+            val result = StringBuilder()
+            var line: String?
+            while (r.readLine().also { line = it } != null) {
+                result.append(line).appendln()
+            }
+            result.toString()
+        } catch (e: IOException) {
+            e.toString()
+        }
+    }
+
+    private fun parseJsonData(data: String): MutableList<QuotesModel> {
+        val list: MutableList<QuotesModel> = Gson().fromJson<MutableList<QuotesModel>>(data)
+        list.forEach {
+            if (it.author == null) {
+                it.author = "Unknown"
+            }
+        }
+        return list
+    }
+
+    private fun parseJsonImage(data: String): MutableList<ImagesModel> {
+        val list: MutableList<ImagesModel> = Gson().fromJson<MutableList<ImagesModel>>(data)
+        return list
+    }
+
+    inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object : TypeToken<T>() {}.type)
+
+    fun getScreenWidth(): String {
+        return Resources.getSystem().displayMetrics.widthPixels.toString()
+    }
+
+    fun getScreenHeight(): String {
+        return Resources.getSystem().displayMetrics.heightPixels.toString()
+    }
+}
